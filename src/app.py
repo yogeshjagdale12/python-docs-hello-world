2ce47653a8aa57fb8a293c110ffe45fae2070d3b
yogeshjagdale12 <87924674+yogeshjagdale12@users.noreply.github.com>
2021-08-15 18:19:34 +1000

    Update and rename app.py to src/app.py

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to GOA singham!! ... Lets Learn DevOps TOGETHER :)"
+    return "Hello, World!"
 
 
 if __name__ == "__main__":
